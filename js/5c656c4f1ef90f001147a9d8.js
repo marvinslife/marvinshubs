3b8917655df755424e830ad53c2080c73d0ee7b1
marvinslife <riley.lefler8@gmail.com>
2024-12-10 19:44:18 -0600

    Add files via upload

---
+++ js/5c656c4f1ef90f001147a9d8.js
@@ -0,0 +1,24 @@
+window.__sharethis__.init({
+    "ts": 1550150789804,
+    "inline-share-buttons": {
+        "enabled": true,
+        "alignment": "center",
+        "color": "social",
+        "font_size": 12,
+        "has_spacing": true,
+        "labels": "counts",
+        "langauge": "en",
+        "min_count": 1,
+        "networks": ["facebook", "twitter", "pinterest", "reddit", "tumblr", "googleplus", "vk", "sharethis"],
+        "num_networks": 8,
+        "padding": 10,
+        "radius": 4,
+        "show_total": true,
+        "size": 32,
+        "size_label": "small",
+        "spacing": 8,
+        "use_native_counts": true,
+        "ts": 1550150789798,
+        "updated_at": 1550150789798
+    }
+});
